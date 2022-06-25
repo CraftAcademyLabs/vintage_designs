3bd41b5728b7ebcc298b3a64a135357f31969ba2
Thomas Ochman <thomas@dragster.se>
2022-06-25 22:38:35 +0200

    changes prototype logo

--- src/Logo.js
+++ src/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image } from '@chakra-ui/react';
-import logo from './vintage_designs.svg';
+import logo from './vintage_designs_2.svg';
 
 export const Logo = (props) => {
   return <Image src={logo} {...props} />;
